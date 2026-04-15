569919632aa3c7268336ae51f7f95ebf2ab96912
Minedscorpion <69402472+MinedscorpionBR@users.noreply.github.com>
2026-04-15 17:08:55 -0300

    Add files via upload

---
+++ atividadeextra.py
@@ -0,0 +1,7 @@
+animaisdeestimacao = ["Cachorro", "Gato", "Peixe"]
+print(f"Lista de animais: {animaisdeestimacao}")
+print(f"Primeiro animal: {animaisdeestimacao[0]}")
+print(f"Ultimo animal: {animaisdeestimacao[2]}")
+animaisdeestimacao.append("ARANHA TARANTULA")
+print(f"novo animal:ARANHA TARANTULA")
+print(f"Após adicionar: {animaisdeestimacao}")
